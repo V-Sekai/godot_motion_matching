03fc267e5ff66e7d0807373c29c4dcce45ee3097
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-01-25 14:23:08 -0800

    Cleanup.

--- src/motion_features.h
+++ src/motion_features.h
@@ -43,9 +43,7 @@ struct MotionFeature : public Resource {
 public:
 	static constexpr float delta = 0.016f;
 
-	MotionFeature() {
-		set_local_to_scene(true);
-	}
+	MotionFeature();
 
 	virtual void physics_update(double delta) {}
 
@@ -65,23 +63,7 @@ public:
 	virtual void debug_pose_gizmo(Ref<EditorNode3DGizmo> gizmo, const PackedFloat32Array data, Transform3D tr = Transform3D{}) { return; }
 
 protected:
-	static void _bind_methods() {
-		ClassDB::bind_method(D_METHOD("get_dimension"), &MotionFeature::get_dimension);
-
-		ClassDB::bind_method(D_METHOD("get_weights"), &MotionFeature::get_weights);
-
-		// BIND_VIRTUAL_METHOD(MotionFeature,get_dimension);
-
-		ClassDB::bind_method(D_METHOD("setup_nodes", "character"), &MotionFeature::setup_nodes);
-
-		ClassDB::bind_method(D_METHOD("setup_for_animation", "animation"), &MotionFeature::setup_for_animation);
-		ClassDB::bind_method(D_METHOD("bake_animation_pose", "animation", "time"), &MotionFeature::bake_animation_pose);
-
-		ClassDB::bind_method(D_METHOD("broadphase_query_pose", "blackboard", "delta"), &MotionFeature::broadphase_query_pose);
-		ClassDB::bind_method(D_METHOD("narrowphase_evaluate_cost", "data_to_evaluate"), &MotionFeature::narrowphase_evaluate_cost);
-
-		ClassDB::bind_method(D_METHOD("debug_pose_gizmo", "gizmo", "data", "root_transform"), &MotionFeature::debug_pose_gizmo);
-	}
+	static void _bind_methods();
 };
 
 #include "scene/3d/physics_body_3d.h"
